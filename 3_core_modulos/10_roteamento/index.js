85664d4f2e1df69316fea0ed19b1ba26e35113c0
Alexandre Paixão <faps.job@gmail.com>
2023-04-19 20:24:16 -0300

    Nodejs

--- 3_core_modulos/10_roteamento/index.js
+++ 3_core_modulos/10_roteamento/index.js
@@ -16,8 +16,12 @@ const server = http.createServer((req, res) => {
         res.write(data)
         return res.end()
         })
-        } else {
-
+    } else {
+        fs.readFile('404.html', function (err, data) {
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write(data)
+        return res.end()
+        })   
             
         }
     }
